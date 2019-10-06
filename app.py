6da57be84ca84c18b7c79c7b9c1baa866d46a08d
kayesmith94 <50887040+kayesmith94@users.noreply.github.com>
2019-10-05 21:40:56 -0400

    .

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
 from flask import Flask
+import nexmo
+
+client = nexmo.Client(key='7d3b3494', secret='vMF3mb7GNxMMDt9s')
 
 app = Flask(__name__)
 
 @app.route('/', methods=["GET"])
 def index():
+    client.send_message({
+        'from': '15859357147',
+        'to': '18134849281',
+        'text': 'Hello from Nexmo',
+    })
     return 'HOMEPAGE AND LOGIN (Doesnt need to be extravagant'
 
 @app.route('/signup', methods=["GET"])
